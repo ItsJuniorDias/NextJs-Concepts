556abfd155e35da87933f05e1096d49b9085f02c
Alexandre Junior <juniordias_@live.com>
2021-02-15 11:17:49 -0300

    chore: Generation Page static

--- concepts/src/pages/index.tsx
+++ concepts/src/pages/index.tsx
@@ -1,9 +1,43 @@
+import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/Home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+
   return (
     <div> 
-      <Title>Hello Junior</Title>
+      <section> 
+        <Title>Product</Title>
+
+        <ul> 
+          {recommendedProducts.map(recommendedProduct =>  (
+            <>
+            <li>
+               {recommendedProduct.title}
+            </li>
+            </>
+          ))}  
+        </ul> 
+      </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended')
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
